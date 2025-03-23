b88aeebb48450450c30bec6ac455e47d0cadac9b
F3700 <albertfebrian26@gmail.com>
2025-03-24 03:51:36 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.CodeDom.Compiler;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999); //diisi bilangan random sepanjang 5 digit
+        this.title = title;
+        this.playCount = 0;
+    }
+    public void IncreasePlayCount(int tambahan)
+    {
+        this.playCount += tambahan;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Title = " + this.title);
+        Console.WriteLine("Play count = " + this.playCount);
+        Console.WriteLine("Id = "+ this.id + "\n");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo vid1 = new SayaTubeVideo("Tutorial Design By Contract – Albert Febrian");
+        vid1.PrintVideoDetails();
+
+        vid1.IncreasePlayCount(1);
+        vid1.IncreasePlayCount(1);
+        vid1.IncreasePlayCount(2);
+
+        vid1.PrintVideoDetails();
+    }
+}
--- obj/Debug/net8.0/tpmodul6_103022300003.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul6_103022300003.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul6_103022300003")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae6ce440ca4f852dcdafe02fb4f652229be70ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul6_103022300003")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul6_103022300003")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
